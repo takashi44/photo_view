2518474124612180c0035013ecaadee5634368f4
takashi44 <takashi44@gmail.com>
2026-01-02 14:33:26 -0800

    Add app bundle and icon assets

--- launch.py
+++ launch.py
@@ -2,6 +2,7 @@
 
 from PySide6 import QtWidgets
 from PySide6 import QtCore
+from PySide6 import QtGui
 import sys
 import photo_view.widget
 
@@ -10,6 +11,8 @@ photo_view.logger.setDebug()
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
+    app.setApplicationName("Photo View")
+    QtGui.QGuiApplication.setApplicationDisplayName("Photo View")
     window = photo_view.widget.PV_MainWindow()
     window.show()
     sys.exit( app.exec_() )
